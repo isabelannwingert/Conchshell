3ce6e3143aa34617104470e160f21cdc0caa1dc9
Sherry Rui Shen <sherry.rui.shen@gmail.com>
2023-09-21 15:09:27 -0400

    adding plotter functions for QA

--- conchshell.py
+++ conchshell.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 
+import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 def read_connmat(txtfile, delimiter=' '):
     return np.genfromtxt(txtfile, delimiter=delimiter)
@@ -72,4 +74,50 @@ def average_intrahemispheric_strength(connmat):
     submatRut = submatR[np.triu_indices(halfn, 1)]
     print(submatLut.shape)
     return np.mean(np.concatenate((submatLut, submatRut)))
-    
+
+def connmat_varations(root, subject_list, atlas_list, file):
+    averaged_across_edges_per_scan = {}
+    averaged_across_scans_per_edge = {}
+    for atlas in atlas_list:
+        for subject in subject_list:
+                txtfile = os.path.join(root, subject, 'Connectomes', '_'.join([subject, atlas, file]))
+                connmat = read_connmat(txtfile)
+                if averaged_across_edges_per_scan.get(atlas) is None:
+                    averaged_across_edges_per_scan[atlas] = [average_network_strength(connmat)]
+                else:
+                    averaged_across_edges_per_scan[atlas].append(average_network_strength(connmat))
+                if averaged_across_scans_per_edge.get(atlas) is None:
+                    averaged_across_scans_per_edge[atlas] = connmat[np.triu_indices(connmat.shape[0],1)] / len(subject_list)
+                else:
+                    averaged_across_scans_per_edge[atlas] = averaged_across_scans_per_edge[atlas] + connmat[np.triu_indices(connmat.shape[0],1)] / len(subject_list)
+    return averaged_across_edges_per_scan, averaged_across_scans_per_edge
+
+def plot_heatmap(connmat, ax=None, title=None, row_labels=None, col_labels=None, colorbar=False, annot=False, **kwargs):
+    if ax is None:
+        ax = plt.gca()
+    im = ax.imshow(connmat, **kwargs)
+    ax.set_xticks(np.arange(connmat.shape[0]))
+    ax.set_yticks(np.arange(connmat.shape[1]))
+    if title is not None:
+        ax.set_title(title)
+    if col_labels is not None:
+        ax.set_xticklabels(col_labels)
+        plt.setp(ax.get_xticklabels(), rotation=30, ha="center", rotation_mode="anchor")
+    if row_labels is not None:
+        ax.set_yticklabels(row_labels)
+    if colorbar:
+        ax.figure.colorbar(im, ax=ax)
+    if annot:
+        for i in range(connmat.shape[0]):
+            for j in range(connmat.shape[1]):
+                ax.text(j, i, int(connmat[i, j]), ha="center", va="center", color="w")
+    return im
+
+
+def plot_hist(connmat, ax=None, title=None, **kwargs):
+    if ax is None:
+        ax = plt.gca()
+    im = ax.hist(connmat[np.triu_indices(connmat.shape[0],1)], **kwargs)
+    if title is not None:
+        ax.set_title(title)
+    return im

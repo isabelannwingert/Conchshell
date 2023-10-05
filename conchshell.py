d4aaa14230d710276cd44dcd406047a32b4d9b16
Sherry-SR <sherry.rui.shen@gmail.com>
2023-10-05 08:56:05 -0400

    QC for multiple atlases

--- conchshell.py
+++ conchshell.py
@@ -18,18 +18,30 @@ DESCRIPTION = '''
 def buildArgsParser():
     p = argparse.ArgumentParser(description=DESCRIPTION)
     
-    p.add_argument('-i', '--input', action='store', metavar='<txt>', dest='inputtxt',
+    p.add_argument('-s', '--subject', action='store', metavar='<txt>', dest='subjecttxt',
                     type=str, required=True, 
-                    help='A two-column comma-delimited txt file containing subjectIDs and paths to each connectome matrix that will undergo examination for quality control'
+                    help='A txt file containing subjectIDs that will undergo examination for quality control'
+                    )
+    p.add_argument('-p', '--path', action='store', metavar='<txt>', dest='pathtxt',
+                    type=str, required=True, 
+                    help='A comma-delimited txt file containing:\n\
+                        column 1 (required): atlas name,\n\
+                        column 2 (required): path template to the connectome file with {s} token for subjectIDs,\n\
+                        column 3 (optional): path template to the nifti atlas in subject space with {s} token for subjectIDs,\n\
+                        column 4 (optional): path to the atlas node order txt file.\n\
+                        Example txt:\n\
+                        desikan,/path/to/{s}_desikan_connmat.txt,/path/to/{s}_desikan.nii.gz,desikan_lut.txt\n\
+                        Schaefer100,/path/to/{s}_Schaefer100_connmat.txt,/path/to/{s}_Schaefer100.nii.gz,schaefer_node_order.txt'
                     )
     p.add_argument('-o', '--output', action='store', metavar='<dir>', dest='outputdir',
                     type=str, required=True, 
                     help='Path to the folder for the outputs'
                     )
-    p.add_argument('-t', '--tck', action='store', metavar='<txt>', dest='tcktxt',
-                   type=str, required=False,
-                   help='The txt file containing a list of paths to folders that store track statistics data (tckstats.txt, nseeds.txt). If not specified, the pipeline will by default search the folder where each connectome matrix is located',
-                   )
+    p.add_argument('-t', '--tck', action='store', metavar='<str>', dest='tckstr',
+                    type=str, required=False,
+                    help='The path template to the folders that store track statistics data (tckstats.txt, nseeds.txt).\
+                        If not specified, the pipeline will by default search the folder where each connectome matrix is located. If not found, skip those measures.'
+                    )
     p.add_argument('-c', '--covars', action='store', metavar='<csv>', dest='covarscsv',
                     type=str, required=False, default=None,
                     help='A csv file containing covariate features (e.g. Age, Group, Site) with subjectID in the leftmost column'
@@ -48,76 +60,91 @@ def main(argv):
     parser = buildArgsParser()
     args = parser.parse_args(argv)
 
-    # Import connectome data and compute QC measures
-    subjlist, connpaths = np.genfromtxt(args.inputtxt, dtype='str', delimiter=',', unpack=True)
-    if args.tcktxt is not None:
-        tckpaths = [r.strip() for r in open(args.tcktxt,'r').readlines()]
-    else:
-        tckpaths = [os.path.dirname(i) for i in connpaths]
+    # Import connectome data
+    subjlist = np.loadtxt(args.subjecttxt, dtype='str', delimiter = '\n')
+    atlaslist, connpaths, *params = np.genfromtxt(args.pathtxt, dtype='str', delimiter=',', unpack=True)
     if not os.path.isdir(args.outputdir):
         os.makedirs(args.outputdir)
-    pp_heatmap = PdfPages(os.path.join(args.outputdir, 'heatmaps.pdf'))
-    pp_histogram = PdfPages(os.path.join(args.outputdir, 'histograms.pdf'))
-
-    measures = {'Subject':[], 'file': [],
-                'density':[], 'num_connected_components':[], 'avg_network_strength':[],
-                'avg_interhemispheric_strength':[], 'avg_intrahemispheric_strength':[],
-                'count':[], 'nseeds':[], 'ratioCN':[],
-                'mean_streamlength':[], 'median_streamlength':[], 'stdev_streamlength':[],
-                'min_streamlength':[], 'max_streamlength':[]
-                }
 
     FIG_ROW = 4
     FIG_COL = 5
-    fig1 = plt.figure(figsize=(25, 20))
-    fig2 = plt.figure(figsize=(25, 20))
-    max_edges = [slstats.read_connmat(connpaths[i]).max() for i in range(len(subjlist))] 
-    max_edges = np.asarray(max_edges)
-    vmax = np.log(np.percentile(max_edges, 99))
-    
-    for i, subject in enumerate(subjlist):
-        print('Checking connectome', i+1, 'out of', len(subjlist))
-        connmat = slstats.read_connmat(connpaths[i])
-        measures['Subject'].append(subject)
-        measures['file'].append(connpaths[i])
-        measures['density'].append(slstats.density(connmat))
-        measures['num_connected_components'].append(slstats.num_conn_comp(connmat))
-        measures['avg_network_strength'].append(slstats.average_network_strength(connmat))
-        measures['avg_interhemispheric_strength'].append(slstats.average_interhemispheric_strength(connmat))
-        measures['avg_intrahemispheric_strength'].append(slstats.average_intrahemispheric_strength(connmat))
+    # compute QC measures for each atlas
+    for t, atlas in enumerate(atlaslist):
+        pp_heatmap = PdfPages(os.path.join(args.outputdir, atlas+'_heatmaps.pdf'))
+        pp_histogram = PdfPages(os.path.join(args.outputdir, atlas+'_histograms.pdf'))
 
-        tckstats_df = slstats.tckstats_statistics(tckpaths[i])
+        measures = {'Subject':[], 'file': [],
+                    'density':[], 'num_connected_components':[], 'avg_network_strength':[],
+                    'avg_interhemispheric_strength':[], 'avg_intrahemispheric_strength':[]
+                    }
+
+        fig1 = plt.figure(figsize=(25, 20))
+        fig2 = plt.figure(figsize=(25, 20))
+        max_edges = [slstats.read_connmat(connpaths[t].format(s=subject)).max() for subject in subjlist] 
+        max_edges = np.asarray(max_edges)
+        vmax = np.log(np.percentile(max_edges, 99))
+        
+        for i, subject in enumerate(subjlist):
+            print('Checking connectome', i+1, 'out of', len(subjlist))
+            connmat = slstats.read_connmat(connpaths[t].format(s=subject))
+            measures['Subject'].append(subject)
+            measures['file'].append(connpaths[t].format(s=subject))
+            measures['density'].append(slstats.density(connmat))
+            measures['num_connected_components'].append(slstats.num_conn_comp(connmat))
+            measures['avg_network_strength'].append(slstats.average_network_strength(connmat))
+            measures['avg_interhemispheric_strength'].append(slstats.average_interhemispheric_strength(connmat))
+            measures['avg_intrahemispheric_strength'].append(slstats.average_intrahemispheric_strength(connmat))
+
+            # Plot heatmaps and histograms to pdf file
+            findex = i%(FIG_COL*FIG_ROW)
+            if  (i > 0) and (findex == 0):
+                pp_heatmap.savefig(fig1)
+                pp_histogram.savefig(fig2)
+                fig1.clf()
+                fig2.clf()
+            ax = fig1.add_subplot(FIG_ROW, FIG_COL, findex+1)
+            plot_heatmap(np.log(connmat+1), ax=ax, title=subject, vmax=vmax)
+            ax = fig2.add_subplot(FIG_ROW, FIG_COL, findex+1)
+            plot_hist(connmat, ax=ax, title=subject, bins=50, log=True)
+
+        fig1.tight_layout()
+        pp_heatmap.savefig(fig1)
+        fig2.tight_layout()
+        pp_histogram.savefig(fig2)
+        pp_heatmap.close()
+        pp_histogram.close()
+
+        # Save QC measures to csv file
+        measures_df = pd.DataFrame(measures)
+        measures_df.to_csv(os.path.join(args.outputdir, atlas+'_QC_measures.csv'))
+
+    #compute QC measures for tckstats.txt and nseeds.txt
+    if args.tckstr is not None:
+        tckpaths = args.tckstr
+    else:
+        tckpaths = os.path.dirname(connpaths[0])
+    measures = {'count':[], 'nseeds':[], 'ratioCN':[],
+                'mean_streamlength':[], 'median_streamlength':[], 'stdev_streamlength':[],
+                'min_streamlength':[], 'max_streamlength':[]
+                }
+    tck_found = True
+    for subject in subjlist:
+        try:
+            tckstats_df = slstats.tckstats_statistics(tckpaths.format(s=subject))
+        except:
+            tck_found = False
+            break
         measures['count'].append(slstats.count(tckstats_df))
-        measures['nseeds'].append(slstats.nseeds(tckpaths[i]))
+        measures['nseeds'].append(slstats.nseeds(tckpaths.format(s=subject)))
         measures['ratioCN'].append(measures['count'][-1] / measures['nseeds'][-1])
         measures['mean_streamlength'].append(slstats.mean_streamlength(tckstats_df))
         measures['median_streamlength'].append(slstats.median_streamlength(tckstats_df))
         measures['stdev_streamlength'].append(slstats.stdev_streamlength(tckstats_df))
         measures['min_streamlength'].append(slstats.min_streamlength(tckstats_df))
         measures['max_streamlength'].append(slstats.max_streamlength(tckstats_df))
-
-        # Plot heatmaps and histograms to pdf file
-        findex = i%(FIG_COL*FIG_ROW)
-        if  (i > 0) and (findex == 0):
-            pp_heatmap.savefig(fig1)
-            pp_histogram.savefig(fig2)
-            fig1.clf()
-            fig2.clf()
-        ax = fig1.add_subplot(FIG_ROW, FIG_COL, findex+1)
-        plot_heatmap(np.log(connmat+1), ax=ax, title=subject, vmax=vmax)
-        ax = fig2.add_subplot(FIG_ROW, FIG_COL, findex+1)
-        plot_hist(connmat, ax=ax, title=subject, bins=50, log=True)
-
-    fig1.tight_layout()
-    pp_heatmap.savefig(fig1)
-    fig2.tight_layout()
-    pp_histogram.savefig(fig2)
-    pp_heatmap.close()
-    pp_histogram.close()
-
-    # Save QC measures to csv file
-    measures_df = pd.DataFrame(measures)
-    measures_df.to_csv(os.path.join(args.outputdir, 'QC_measures.csv'))
+    if tck_found:
+        measures_df = pd.DataFrame(measures)
+        measures_df.to_csv(os.path.join(args.outputdir, 'tck_QC_measures.csv'))
     
     #TO DO: adjust for covariates and compute z-scores
     # z-score columns: density, avg_network_strength, avg_interhemispheric_strength, avg_intrahemispheric_strength, 
